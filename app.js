01cdd9396dca21292791a451acd4c3acef68350b
Bernard El-Hagin <bernard.elhagin@gmail.com>
2024-05-13 10:48:02 +0200

    EJS done

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const express = require('express');
+const mariadb = require('mariadb');
 
 const server = express();
+
+const pool = mariadb.createPool({
+   host: 'localhost',
+   database: 'finkployd',
+   user: 'bertold',
+   password: 'bertold',
+   connectionLimit: 5
+});
 
 server.use(express.static('./public/'));
 
 server.set('views', './views');
 server.set('view engine', 'ejs');
 
+let albums = [];
+let merch = [];
+
+async function getData() {
+   let connection;
+   try {
+      connection = await pool.getConnection();
+      albums = await connection.query("SELECT id, item_name, item_price, item_image FROM shop_items WHERE item_type = 'A'");
+      merch  = await connection.query("SELECT id, item_name, item_price, item_image FROM shop_items WHERE item_type = 'M'");
+   } catch (error) {
+      console.log('Error: ' + error);
+   }
+}
+
+getData();
+
 server.get('/store', (request, response) => {
-   response.render('store');
+   console.log(merch);
+   response.render('store', { albums: albums, merch: merch });
 });
 
 server.listen(3000, () => {
